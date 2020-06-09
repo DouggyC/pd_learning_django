7db304595933afe19c8115aaa85e1b537448f7ec
Douglas <douglas@mediushealth.org>
2020-06-09 18:15:54 +1000

    render listings from database

--- listings/views.py
+++ listings/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
+from .models import Listing
+
 def index(request):
-  return render(request, 'listings/listings.html')
+  listings = Listing.objects.all()
+
+  context = {
+    'listings': listings
+  }
+
+  return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-  return render(request, 'listing/listing.html')
+  return render(request, 'listings/listing.html')
 
 def search(request):
-  return render(request, 'listing/search.html')
+  return render(request, 'listings/search.html')
