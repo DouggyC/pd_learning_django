e4618e1a929c68a784ab68e495a31ba98735b2b6
Douglas <douglas@mediushealth.org>
2020-06-19 14:26:52 +1000

    contacts admin customization, contact form prep, contact form submission

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from operator import itemgetter
+
+from .models import Contact
 
 # Create your views here.
+def contact(request):
+  if request.method == "POST":
+    listing_id, listing, name, email, phone, message, user_id, realtor_email = itemgetter('listing_id', 'listing', 'name', 'email', 'phone', 'message', 'user_id', 'realtor_email')(request.POST)
+
+    contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
+    contact.save()
+
+    messages.success(request, 'Thank you, your enquiry has been sent.')
+    return redirect('/listings/'+listing_id)
