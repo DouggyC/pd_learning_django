9cf09ce258b26eddcd7d1f512e2ee917258df3f0
Douglas <douglas@mediushealth.org>
2020-06-22 13:53:15 +1000

    clean settings for prod

--- btre/settings.py
+++ btre/settings.py
@@ -144,13 +144,6 @@ MESSAGE_TAGS = {
     messages.SUCCESS: 'success'
 }
 
-# Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'kristian.y100@gmail.com'
-EMAIL_HOST_PASSWORD = 'MakeitRain890'
-EMAIL_USE_TLS = True
-
 try:
     from .local_settings import *
 except ImportError:
